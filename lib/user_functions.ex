a8a3fd7ae6208e98186b9429a0408bd1436c5e17
Sam Woolerton <samuelwoolerton@gmail.com>
2020-02-09 19:21:15 +1300

    Propagate function argument error terms instead of replacing with own error

--- lib/user_functions.ex
+++ lib/user_functions.ex
@@ -4,33 +4,39 @@ defmodule Parser.UserFunctions do
   end
 
   def functions do
-    functions = %{
+    %{
       "sum" => {
         fn ls ->
-          if all_numbers(ls) do
-            ls
-            |> Enum.map(&extract(:number, &1))
-            |> Enum.sum
-            |> ok(:number)
-          else
-            {:error, "sum only works on numbers"}
+          case first_error(ls) do
+            nil ->
+              if all_numbers?(ls) do
+                ls
+                |> Enum.map(&extract(:number, &1))
+                |> Enum.sum()
+                |> ok(:number)
+              else
+                {:error, "sum only works on numbers"}
+              end
+
+            err ->
+              err
           end
         end
       },
       "join" => {
-        fn
-          [{:error, message} | _] ->
-            {:error, message}
+        fn ls ->
+          case first_error(ls) do
+            nil ->
+              if all_strings?(ls) do
+                [sep | strings] = ls |> Enum.map(&extract(:string, &1))
+                strings |> Enum.join(sep) |> ok(:string)
+              else
+                {:error, "Arguments to join must all be strings"}
+              end
 
-          [{:ok, {:string, sep}} | ls] ->
-            if all_strings(ls) do
-              ls |> Enum.map(&extract(:string, &1)) |> Enum.join(sep) |> ok(:string)
-            else
-              {:error, "Arguments to join must all be strings"}
-            end
-
-          [{:ok, _} | _] ->
-            {:error, "Join separator must be a string"}
+            err ->
+              err
+          end
         end,
         %{
           min: 3
@@ -43,11 +49,16 @@ defmodule Parser.UserFunctions do
     {:ok, {atom, value}}
   end
 
-  def all_strings(ls), do: is_all?(ls, :string)
+  def first_error(ls), do: Enum.find(ls, &is_error?(&1))
 
-  def all_numbers(ls), do: is_all?(ls, :number)
+  def is_error?({:error, _}), do: true
+  def is_error?(_), do: false
 
-  def all_booleans(ls), do: is_all?(ls, :boolean)
+  def all_strings?(ls), do: is_all?(ls, :string)
+
+  def all_numbers?(ls), do: is_all?(ls, :number)
+
+  def all_booleans?(ls), do: is_all?(ls, :boolean)
 
   def is_all?(ls, atom) do
     Enum.all?(ls, fn
