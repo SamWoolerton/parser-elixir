36787d9d6e0abaa84092efa793b5bfc9e4aea045
Sam Woolerton <samuelwoolerton@gmail.com>
2020-02-09 18:46:28 +1300

    Add join user function, refactor shared logic

--- lib/user_functions.ex
+++ lib/user_functions.ex
@@ -7,20 +7,57 @@ defmodule Parser.UserFunctions do
     functions = %{
       "sum" => {
         fn ls ->
-          List.foldl(ls, {:ok, {:number, 0}}, fn
-            {:error, message}, _ -> {:error, message}
-            _, {:error, message} -> {:error, message}
-            {:ok, {:number, next}}, {:ok, {:number, acc}} -> {:ok, {:number, acc + next}}
-            _, _ -> {:error, "sum only works on numbers"}
-          end)
+          if all_numbers(ls) do
+            ls
+            |> Enum.map(&extract(:number, &1))
+            |> Enum.sum
+            |> ok(:number)
+          else
+            {:error, "sum only works on numbers"}
+          end
         end
       },
       "join" => {
-        fn [separator | ls] -> {:ok, {:string, "joined"}} end,
+        fn
+          [{:error, message} | _] ->
+            {:error, message}
+
+          [{:ok, {:string, sep}} | ls] ->
+            if all_strings(ls) do
+              ls |> Enum.map(&extract(:string, &1)) |> Enum.join(sep) |> ok(:string)
+            else
+              {:error, "Arguments to join must all be strings"}
+            end
+
+          [{:ok, _} | _] ->
+            {:error, "Join separator must be a string"}
+        end,
         %{
           min: 3
         }
       }
     }
+  end
+
+  def ok(value, atom) do
+    {:ok, {atom, value}}
+  end
+
+  def all_strings(ls), do: is_all?(ls, :string)
+
+  def all_numbers(ls), do: is_all?(ls, :number)
+
+  def all_booleans(ls), do: is_all?(ls, :boolean)
+
+  def is_all?(ls, atom) do
+    Enum.all?(ls, fn
+      {:ok, {^atom, _}} -> true
+      _ -> false
+    end)
+  end
+
+  def extract(atom, ast) do
+    {:ok, {^atom, a}} = ast
+    a
   end
 end
